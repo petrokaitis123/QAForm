e4c2735fc8d1646fabea80ec8c439641aec64f4b
petrokaitis123 <petrokaitis123@github.com>
2023-10-18 18:56:19 +0100

    Set up spring boot project

---
+++ QAForm/src/main/java/qa/form/QaFormApplication.java
@@ -0,0 +1,13 @@
+package qa.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaFormApplication.class, args);
+	}
+
+}
---
+++ QAForm/src/test/java/qa/form/QaFormApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
